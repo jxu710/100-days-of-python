52f61b20fb11eb1c9841d45958383ed253b6d5ec
Replit user <>
2023-08-22 14:34:53 +0000

    Add day1's project, band name

---
+++ day1/main.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+print("hello! It's day 1 for 100-days-Python")
+
+city = input("What is the name of the city you grew up\n")
+pet = input("What is the name of your fist pet\n")
+
+print("Your new band name is : " + city + " " + pet)
