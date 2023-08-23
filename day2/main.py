bc8672c7bb0080f0d3a95a0d75638e5501e6ab42
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-23 03:54:55 +0000

    submit my answer for day 2 project --tip calculator

--- day2/main.py
+++ day2/main.py
@@ -7,3 +7,15 @@
 
 #Write your code below this line 👇
 
+print("Welcome to the tip calculator")
+
+totalBill = float(input("What is the total bill cost? "))
+tipPercent = float(input("what percentage of tips u would like to give, 10, 12, or 15 ? "))
+num_of_people = float(input("how many people to split the bill? "))
+try:
+  singleBill = (totalBill / num_of_people) * (1+ tipPercent/100)
+  roundedNumber = round(singleBill, 2)
+  print("Each person should pay $",roundedNumber)
+except:
+  print("Invalid Input, please type a valid number")
+  quit()
