91e042039f4a2ba87d75550b1297688545f702a3
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-26 06:28:40 +0000

    Add self-created module

---
+++ day4/random/random_example.py
@@ -0,0 +1,13 @@
+
+# import random module to use for generating random stuff
+import random 
+
+# import module from my_module , the module I created 
+import my_module
+
+random_interger = random.randint(1,10)
+
+print(random_interger)
+
+
+print(my_module.date)
--- day4/random_example.py
+++
@@ -1,7 +0,0 @@
-
-# import random module to use for generating random stuff
-import random 
-
-random_interger = random.randint(1,10)
-
-print(random_interger)
