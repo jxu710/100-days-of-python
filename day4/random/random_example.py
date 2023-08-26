5bc9aaeea3bb65db0f331ad703bf9850bf02a8ae
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-26 06:34:56 +0000

    random number updated

--- day4/random/random_example.py
+++ day4/random/random_example.py
@@ -5,9 +5,17 @@ import random
 # import module from my_module , the module I created 
 import my_module
 
+# random interget between 1 and 10, inclusively 
 random_interger = random.randint(1,10)
 
 print(random_interger)
 
 
 print(my_module.date)
+
+# random float number between 0 and 1, not include 0 and 1 
+random_float = random.random()
+print(random_float)
+
+# 0 ... to 4.999999
+random_float * 5 
