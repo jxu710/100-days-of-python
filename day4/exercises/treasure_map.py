b2baa7ed5c006322ffa32826351a5e06f569de6b
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-27 22:41:13 +0000

    Add my solution for treasure map practice

---
+++ day4/exercises/treasure_map.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+
+# extract the first index number
+first_index = int(position[0])
+
+# extract the second index number
+second_index = int(position[1])
+
+# find the position in the nested list
+map[second_index-1][first_index-1] = "X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
