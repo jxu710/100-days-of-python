1de085523ff2c63d0aae03eaf37a8cbafc27ee04
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-27 20:44:16 +0000

    Added another solution using choice method

--- day4/random/banker_roulette.py
+++ day4/random/banker_roulette.py
@@ -10,3 +10,8 @@ names = names_string.split(", ")
 random_int = random.randint(0,len(names)-1)
 
 print(f"{names[random_int]} is going to buy the meal today!")
+
+# Another solution, using choice method
+
+person_who_will_pay = random.choice(names)
+print(person_who_will_pay + " is going to buy the meal today!")
