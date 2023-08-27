db6ebaced5797460a851351770c568de404679b4
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-27 23:34:42 +0000

    Add my solution for the rock paper scissor game

--- day4/main.py
+++ day4/main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -27,3 +29,38 @@ scissors = '''
 
 #Write your code below this line 👇
 
+user_choice = int(input('"What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. '))
+
+if user_choice == 0:
+  print(rock)
+elif user_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+
+
+# generate computer choice here 
+computer_choice = random.randint(0,2)
+if computer_choice == 0:
+  print(f"Computer chose:\n {rock}")
+elif computer_choice == 1:
+  print(f"Computer chose:\n {paper}")
+else:
+  print(f"Computer chose:\n {scissors}")
+
+# Determine who win the game 
+
+if user_choice == computer_choice:
+  print("It's a tie")
+elif user_choice == 0 and computer_choice ==1:
+  print("You lose")
+elif user_choice == 0 and computer_choice ==2:
+  print("You win")
+elif user_choice ==1 and computer_choice ==0:
+  print("You win")
+elif user_choice ==1 and computer_choice ==2:
+  print("You lose")
+elif user_choice ==2 and computer_choice ==0:
+  print("You lose")
+else:
+  print("You win")
