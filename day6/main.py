b807917736d821824a2ba27edf1ab5c62915d677
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-30 08:15:13 +0000

    Add code to fix the infinite loop issue  the Maze challenge might cause

--- day6/main.py
+++ day6/main.py
@@ -18,6 +18,29 @@ while not at_goal():
         
 
 
+#  solution to fix the infinite loop bug 
+# remember if robot in a position that has wall on the right, it will never get into infinite loop 
+def turn_right():
+
+    turn_left()
+
+    turn_left()
+
+    turn_left()
+
+while front_is_clear():
+    move()
+   
+
+while not at_goal():
+    if front_is_clear():
+        move()
+        if right_is_clear():
+            turn_right()
+    else:
+        turn_left()
+
+
 
 
     
