2353c2ce37e408d88477916bec2eebe1283b7e63
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-08-30 03:55:17 +0000

    Add my solution for hurdle 4 Reeborg's World challenge

---
+++ day6/exercises/hurdle4.py
@@ -0,0 +1,23 @@
+# This is my solution for the Hurdle 4 challenge at Reeborg's World:  https://reeborg.ca/index_en.html
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+       jump()
+    else:
+        move()
+    
