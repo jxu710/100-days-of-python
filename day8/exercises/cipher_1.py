a41a9a15b50673c1cb9ed4867dd1697cd4d09626
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-09-02 04:05:03 +0000

    Add another solution for 1st of cipher project

--- day8/exercises/cipher_1.py
+++ day8/exercises/cipher_1.py
@@ -31,3 +31,15 @@ def encrypt(text,shift):
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 
 encrypt(text,shift)
+
+# instructor's solution 
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = (position + shift) % 26
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
