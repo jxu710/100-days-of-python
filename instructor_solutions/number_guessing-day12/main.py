29e87a499c01a296cc151e30335e47ea0ee36291
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-09-27 04:21:01 +0000

    Add rest of the code of the guessing number game

--- instructor_solutions/number_guessing-day12/main.py
+++ instructor_solutions/number_guessing-day12/main.py
@@ -3,18 +3,23 @@ from random import randint
 
 
 #  A Function to check user's number against actual answer.
-def check_answer(guess,answer):
+EASY_LEVEL_TURNS = 10 
+HARD_LEVEL_TURNS = 5
+
+def check_answer(guess,answer,turns):
+  """check answer against guess, Returns the number of turns remaining"""
   if guess > answer:
     print("too high")
+    return turns -1 
   elif guess < answer:
     print("too low")
+    return turns -1 
   else:
     print(f"You got it! the answer is {answer}")
 
 #  Make function to set difficulty 
 
-EASY_LEVEL_TURNS = 10 
-HARD_LEVEL_TURNS = 5
+
 def set_difficulty():
   level = input("choose a difficulty, easy or hard: ")
 
@@ -31,15 +36,18 @@ def game():
   print(f"Pssst, the correct answer is {answer}") 
   
   turns = set_difficulty()
-  print(f"you have {turns} to guess the number")
-  
+ 
   #  Repeat the guessing functionality if they get it right
   guess = 0
   while guess != answer:
+    print(f"you have {turns} to guess the number")
+  
     # Let user guess the number
     guess = int(input("Make a guess: "))
-    check_answer(guess,answer)
-  
+    turns = check_answer(guess,answer,turns)
+    if turns == 0:
+      print("You've run out of guesses, you lose.")
+      return
 #  Track the number of turns and reduce by 1 if they get it wrong 
 
 game()
