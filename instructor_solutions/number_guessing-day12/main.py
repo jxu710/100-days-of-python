0ae8e7f038ad64a147c50b56db48985368dac8c4
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-09-27 03:28:24 +0000

    Add 2nd part of logic

--- instructor_solutions/number_guessing-day12/main.py
+++ instructor_solutions/number_guessing-day12/main.py
@@ -1,6 +1,5 @@
-# Choosing a random number between 1 and 100 
 from random import randint 
-answer = randint(1,100)
+
 
 
 #  A Function to check user's number against actual answer.
@@ -24,13 +23,23 @@ def set_difficulty():
   else:
     return HARD_LEVEL_TURNS
 
-# Let user guess the number 
-turns = set_difficulty()
-guess = int(input("Make a guess: "))
-print(f"you have {turns} to guess the number")
-
-
+def game():
+  # Choosing a random number between 1 and 100 
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+  answer = randint(1,100)
+  print(f"Pssst, the correct answer is {answer}") 
+  
+  turns = set_difficulty()
+  print(f"you have {turns} to guess the number")
+  
+  #  Repeat the guessing functionality if they get it right
+  guess = 0
+  while guess != answer:
+    # Let user guess the number
+    guess = int(input("Make a guess: "))
+    check_answer(guess,answer)
+  
 #  Track the number of turns and reduce by 1 if they get it wrong 
 
-#  Repeat the guessing functionality if they get it right
-
+game()
