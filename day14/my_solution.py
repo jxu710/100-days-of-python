1d209b061b3aa03bb33b62c7ccb6e0f4e539fb7e
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-10-04 00:44:55 +0000

    Add main logic of the game

--- day14/my_solution.py
+++ day14/my_solution.py
@@ -6,11 +6,35 @@ import random
 
 print(logo)
 
-option_a = random.choice(data)
-option_b = random.choice(data)
+def game():
+  point = 0
+  end_game = False
+  
+  while not end_game:
+    option_a = random.choice(data)
+    option_b = random.choice(data)
+    print(f"Compare A:{option_a['name']}, a {option_a['description']} , from {option_a['country']}")
+    print(vs)
+    print(f"Compare B:{option_b['name']}, a {option_b['description']} , from {option_b['country']}")
+    guess = input("Who has more followers? Type 'A' or 'B': ").capitalize()
+    if guess == "A" and option_a["follower_count"] > option_b["follower_count"]:
+      # compare option_a 's follower number with b's follower number 
+      point = point +1
+      clear()
+      print(f"u right,current score {point}")
 
-print(f"Compare A:{option_a['name']}, a {option_a['description']} , from {option_a['country']}")
-print(vs)
-print(f"Compare B:{option_b['name']}, a {option_b['description']} , from {option_b['country']}")
+    elif guess == "B" and option_b["follower_count"] > option_a["follower_count"]:
+      point = point +1
+      clear()
+      print(f"u right,current score {point}")
 
-input("Who has more followers? Type 'A' or 'B': ")
+    elif guess == "A" and option_a["follower_count"] < option_b["follower_count"]:
+      clear()
+      print(f"Sorry, that's wrong. Final score: {point}")
+      end_game = True
+      
+    elif guess == "B" and option_a["follower_count"] > option_b["follower_count"]:
+      clear()
+      print(f"Sorry, that's wrong. Final score: {point}")
+      end_game = True
+game()
