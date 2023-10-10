ca41d37f3b6a6340b74cc7bc115e64dd935c5a29
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-10-10 00:52:56 +0000

    Add instructor solution for higher lower game

--- instructor_solutions/higher_lower_game/main.py
+++ instructor_solutions/higher_lower_game/main.py
@@ -1,36 +1,70 @@
-from art import logo 
+from art import logo, vs
 from game_data import data 
 import random 
-# display art
-print(logo)
-
+from replit import clear
 
 def format_data(account):
-  '''format the account data into printable format '''
+  '''take the account data and returns the printable format '''
   account_name = account["name"]
   account_descr = account["description"]
   account_country = account["country"]
 
   return f"{account_name}, a {account_descr}, from {account_country}"
 
-# generate a random account from the game data 
-account_a = random.choice(data)
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+  
+# display art
+print(logo)
+score = 0
 account_b = random.choice(data)
-if account_a == account_b:
-  account_b = random.choice(data)
-
-print(f"Compare A : {format_data(account_a)}")
-print(f"Compare B : {format_data(account_b)}")
-
-# ask user for a guess
-
-# check if user is correct 
-## get follower count of each account
-## use if statement to check if user if correct 
-
-# give user feedback on their guess 
 # make the game repeatable
+game_should_continue = True
 
-# making account at position B become the next account at position A
+while game_should_continue:
+  # generate a random account from the game data 
 
-# clear the screen
+  # making account at position B become the next account at position A
+
+  account_a = account_b
+  account_b = random.choice(data)
+  
+  while account_a == account_b:
+    account_b = random.choice(data)
+  
+  print(f"Compare A : {format_data(account_a)}")
+  print(vs)
+  print(f"Against B : {format_data(account_b)}")
+  
+  # ask user for a guess
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+  
+  # check if user is correct 
+  ## get follower count of each account
+  
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  
+  is_correct = check_answer(guess, a_follower_count,b_follower_count)
+
+  # clear the screen between rounds
+  clear()
+  print(logo)
+  # give user feedback on their guess 
+  # Score keeping
+  if is_correct: 
+    score +=1 
+    print(f"You're right! Current score: {score}")
+  else:
+    game_should_continue = False
+    print(f"Sorry, that's wrong. Final score: {score}")
+  
+  
+  
